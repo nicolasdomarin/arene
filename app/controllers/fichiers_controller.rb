ef845240476a7fc9f55fb395b0dee5ca46cf02ab
Thomas <t.massonn@gmail.com>
2018-10-03 16:37:53 +0200

    Create file
    Save files into root/csv

--- app/controllers/fichiers_controller.rb
+++ app/controllers/fichiers_controller.rb
@@ -7,6 +7,13 @@ class FichiersController < ApplicationController
   def create
     @fichier = Fichier.new(fichier_params)
     if @fichier.save
+      f = params[@fichier]
+      if f && f.doc_fil_size > 0
+        path = "#{RAILS_ROOT}/csv"
+        File.open("#{path}", "w") do |file|
+          file.write(f.read)
+        end
+      end
       flash[:success] = "Fichier ajouté!"
       redirect_to root_path
     else
